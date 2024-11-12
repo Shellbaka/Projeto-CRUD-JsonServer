8ab503ed8f5fd415d29ceb4b868a399ecea845ef
Shellbaka <shellbaka69@gmail.com>
2024-11-11 22:32:52 -0300

    Quase todas as alterações pendentes foram ajeitadas, foi incrementado a palavra certa dentro de db.json estava pages e era posts, isso estava conflitando o endereço final da aplicação, depois ajeitei para ter uma atualização em setPosts com ( response.data) e coloquei um console para um feedback visual

--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -1,7 +1,23 @@
-import { Card } from '../../components/Card'
-import './styles.css'
+import { useEffect, useState } from "react";
+import { Card } from "../../components/Card";
+import "./styles.css";
+import { api } from "../../lib/axios";
 
 export function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    api
+      .get("/posts")
+      .then((response) => {
+        console.log(response.data);
+        setPosts(response.data);
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar posts:", error);
+      });
+  }, []);
+
   return (
     <div className="feedContainer">
       <Card />
