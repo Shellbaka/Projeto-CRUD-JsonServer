5d95d6d5ce9c6b2f2d164622b623769da0043728
Shellbaka <shellbaka69@gmail.com>
2024-11-11 21:39:31 -0300

    Modificações foram feitas na estilização styles.css, acrescentei todas as totas para o Router, Criei o Axios para a utilização da API e poder começar a fazer as requisições CRUD.

---
+++ src/Router.jsx
@@ -0,0 +1,19 @@
+ import {Routes, Route } from 'react-router-dom';
+ import {Feed} from "./pages/Feed";
+ import {CreatePost} from "./pages/CreatePost";
+ import {UpdatePost} from "./pages/UpdatePost";
+ import {OnePost} from "./pages/OnePost";
+
+
+export function Router ( ) {
+return(
+
+<Routes>
+    <Route path="/" element={<Feed/>} />
+    <Route path="/createPost" element={<CreatePost/>} />
+    <Route path="/updatePost" element={<UpdatePost/>} />
+    <Route path="/onePost" element={<OnePost/>} />
+</Routes>
+);
+
+};
