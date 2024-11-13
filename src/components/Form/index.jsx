c8ba5b01e340bbaec8353fc65d75210a0e3320b8
Shellbaka <shellbaka69@gmail.com>
2024-11-13 17:16:22 -0300

    Trouxe o import do useNavigate usando o react-router-dom para que eu pudesse utilizar da bibliteca para navegar entre páginas, usei a navageção para assim que fizesse o fromulário eu fosse redirecionado para a página de feed, a página principal, assim tendo uma ação dinâmica, mas poderia ser qualquer página.

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -4,20 +4,25 @@ import { api } from "../../lib/axios";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { MdDescription } from "react-icons/md";
+import {useNavigate} from 'react-router-dom'
 
 const postSchema = yup.object({
-  title: yup.string().required(),
-  description: yup.string().required(),
-  content: yup.string().required(),
+  title: yup.string().required('Informe um título'),
+  description: yup.string().required('Acrescente uma descrição'),
+  content: yup.string().required('Coloque o conteúdo'),
 });
 
 export function Form({ title, textButton }) {
-  const { register, handleSubmit, reset } = useForm({
+
+  const navigate=useNavigate();
+
+  const { register, handleSubmit, reset, formState:{errors} } = useForm({
     resolver: yupResolver(postSchema),
   });
   function handleCreatePost(data) {
     api.post("./posts", data);
     console.log("Criado com sucesso");
+    navigate('/');
     reset();
   }
 
@@ -26,14 +31,17 @@ export function Form({ title, textButton }) {
       <h2>{title}</h2>
       <div className="field">
         <input placeholder="Título" {...register("title")} />
+        {errors.title?.message}
       </div>
 
       <div className="field">
         <input placeholder="Descrição" {...register("description")} />
+        {errors.description?.message}
       </div>
 
       <div className="field">
         <textarea placeholder="Conteúdo" {...register("content")} />
+        {errors.content?.message}
       </div>
 
       <button type="submit">{textButton}</button>
