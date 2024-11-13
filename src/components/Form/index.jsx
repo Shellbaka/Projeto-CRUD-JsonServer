9251aefcc1a8271942bbba07d5250d803e0cbf3a
Shellbaka <shellbaka69@gmail.com>
2024-11-13 16:20:50 -0300

    Acrescentei uma nova biblioteca, a hookform/resolvers yup, desta maneira com ele eu posso monitorar se os dados estão indo corretamente para a tabela sem estarem com lacunas vazias, é uma biblioteca que me permite estar fazendo requires para dizer qual tipo de dado eu quero dentro de umna tabela e se eu quero que precise ter dados naquela tabela, uma maneira avançada com crud de fazer validação de dados.

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,30 +1,42 @@
 import "./styles.css";
 import { useForm } from "react-hook-form";
+import { api } from "../../lib/axios";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { MdDescription } from "react-icons/md";
 
-export function Form({title, textButton}) {
-  
-  const {register, handleSubmit, reset} = useForm ()
-  function handleCreatedPost (data) {
-    console.log("Post criado!!!" , data);
+const postSchema = yup.object({
+  title: yup.string().required(),
+  description: yup.string().required(),
+  content: yup.string().required(),
+});
+
+export function Form({ title, textButton }) {
+  const { register, handleSubmit, reset } = useForm({
+    resolver: yupResolver(postSchema),
+  });
+  function handleCreatePost(data) {
+    api.post("./posts", data);
+    console.log("Criado com sucesso");
     reset();
   }
 
   return (
-    <form onSubmit={handleSubmit(handleCreatedPost)}>
+    <form onSubmit={handleSubmit(handleCreatePost)}>
       <h2>{title}</h2>
       <div className="field">
-        <input placeholder="Título" {...register("title")}/>
+        <input placeholder="Título" {...register("title")} />
       </div>
 
       <div className="field">
-        <input placeholder="Descrição" {...register("description")}/>
+        <input placeholder="Descrição" {...register("description")} />
       </div>
 
       <div className="field">
-        <textarea placeholder="Conteúdo" />
+        <textarea placeholder="Conteúdo" {...register("content")} />
       </div>
 
-      <button>{textButton}</button>
+      <button type="submit">{textButton}</button>
     </form>
   );
 }
